0f859b6807e3cd20c0b9b5413f2c2e42588e98cf
DerDeterminist <l.determann@t-online.de>
2019-04-28 14:14:01 +0200

    image board

--- app/src/main/java/com/example/quantumchessapp/Activities/GameActivity.java
+++ app/src/main/java/com/example/quantumchessapp/Activities/GameActivity.java
@@ -4,16 +4,41 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.GridView;
+import com.example.quantumchessapp.GameManager;
 import com.example.quantumchessapp.R;
+import com.example.quantumchessapp.figuren.Piece;
+import com.example.quantumchessapp.spiel.Player;
+import com.example.quantumchessapp.spiel.Position;
 
 public class GameActivity extends AppCompatActivity
 {
+
    @Override
    protected void onCreate(@Nullable Bundle savedInstanceState)
    {
       super.onCreate(savedInstanceState);
 
       setContentView(R.layout.activity_game);
+      GridView gridView = findViewById(R.id.grid);
+      gridView.setNumColumns(GameManager.getWight());
+
+      gridView.addView(new Piece(this,new Position(1,1),new Player(),1, Piece.PieceColor.BLACK, Piece.PieceType.Rook));
+      gridView.addView(new Piece(this,new Position(1,1),new Player(),1, Piece.PieceColor.BLACK, Piece.PieceType.Rook));
+      gridView.addView(new Piece(this,new Position(1,1),new Player(),1, Piece.PieceColor.BLACK, Piece.PieceType.Rook));
+      gridView.addView(new Piece(this,new Position(1,1),new Player(),1, Piece.PieceColor.BLACK, Piece.PieceType.Rook));
+      gridView.addView(new Piece(this,new Position(1,1),new Player(),1, Piece.PieceColor.BLACK, Piece.PieceType.Rook));
+      gridView.addView(new Piece(this,new Position(1,1),new Player(),1, Piece.PieceColor.BLACK, Piece.PieceType.Rook));
+      gridView.addView(new Piece(this,new Position(1,1),new Player(),1, Piece.PieceColor.BLACK, Piece.PieceType.Rook));
+      gridView.addView(new Piece(this,new Position(1,1),new Player(),1, Piece.PieceColor.BLACK, Piece.PieceType.Rook));
+      gridView.addView(new Piece(this,new Position(1,1),new Player(),1, Piece.PieceColor.BLACK, Piece.PieceType.Rook));
+      gridView.addView(new Piece(this,new Position(1,1),new Player(),1, Piece.PieceColor.BLACK, Piece.PieceType.Rook));
+      gridView.addView(new Piece(this,new Position(1,1),new Player(),1, Piece.PieceColor.BLACK, Piece.PieceType.Rook));
+      gridView.addView(new Piece(this,new Position(1,1),new Player(),1, Piece.PieceColor.BLACK, Piece.PieceType.Rook));
+      gridView.addView(new Piece(this,new Position(1,1),new Player(),1, Piece.PieceColor.BLACK, Piece.PieceType.Rook));
+      gridView.addView(new Piece(this,new Position(1,1),new Player(),1, Piece.PieceColor.BLACK, Piece.PieceType.Rook));
+      gridView.addView(new Piece(this,new Position(1,1),new Player(),1, Piece.PieceColor.BLACK, Piece.PieceType.Rook));
+      gridView.addView(new Piece(this,new Position(1,1),new Player(),1, Piece.PieceColor.BLACK, Piece.PieceType.Rook));
    }
 
    @Override
